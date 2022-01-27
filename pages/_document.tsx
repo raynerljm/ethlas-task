3804b4ced4c48adb294febb89e587e61c32f53aa
raynerljm <raynerljm@gmail.com>
2022-01-27 14:20:43 +0800

    Install Overpass font

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import MyHead from "../components/MyHead";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <MyHead />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Overpass:wght@100;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
