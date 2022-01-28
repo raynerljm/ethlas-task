6a0dd480f2abf94d33ad55ae7ba75a5b572b2a26
raynerljm <raynerljm@gmail.com>
2022-01-28 15:29:25 +0800

    Reinstall prisma client and re-generate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
 /**
  * Sets a new prisma client as the global object.
  * Done to maintain a singleton pattern so that the database is not exhausted
  * by multiple prisma connections.
  */
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 export const prisma = global.prisma || new PrismaClient();
 
