ab1194ec61c614306bf8f83675d52ffb1ad1cbfd
raynerljm <raynerljm@gmail.com>
2022-01-28 13:46:07 +0800

    Add gradient to background

--- components/Layout/Body.tsx
+++ components/Layout/Body.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const Body: FC = ({ children }) => {
   return (
     <>
-      <main className="min-h-screen w-full bg-ethlas-black">
+      <main className="min-h-screen w-full bg-gradient-black">
         <div className="max-w-7xl w-full px-4">{children}</div>
       </main>
     </>
